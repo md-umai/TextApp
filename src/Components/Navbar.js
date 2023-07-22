06603c18bcf21abf3bd38037146030dc6209e111
Mohammed Umair <mdumair786114@gmail.com>
2023-07-22 20:13:58 +0530

    First Commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = (props) => {
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
               onClick={() => setShow(!show)}>
               <span class="navbar-toggler-icon"></span>
-            </button>
+            </button> 
             <div class={`collapse navbar-collapse  ${show ? "show" : ""}`} >
               <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item">
